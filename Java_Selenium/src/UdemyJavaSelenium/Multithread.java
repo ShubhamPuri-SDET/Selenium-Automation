8c64576e054db0d5bba45bfa74c4134b275b126a
ShubhamPuri-SDET <Spuri4867@gmail.com>
2025-01-23 19:40:28 +0530

    Update the Multithreading Code

--- Java_Selenium/src/UdemyJavaSelenium/Multithread.java
+++ Java_Selenium/src/UdemyJavaSelenium/Multithread.java
@@ -1,26 +1,49 @@
 package UdemyJavaSelenium;
 
-import org.apache.poi.ss.usermodel.*;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-
-import java.io.*;
-import java.util.*;
-
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Multithread {
 
     public static void main(String[] args) throws IOException {
         List<String> urls = readUrlsFromExcel("C:\\Users\\shubham.puri\\Desktop\\TestWeb\\MulithreadInput.xlsx");
-        int numberOfThreads = 2;
+        int numberOfThreads = 5;
+        List<Map<String, String>> outputData = new ArrayList<>();
 
         // Create and start threads
+        List<Thread> threads = new ArrayList<>();
         for (int i = 0; i < numberOfThreads; i++) {
-            Thread thread = new Thread(new BrowserTask(urls, numberOfThreads, i));
+            Thread thread = new Thread(new BrowserTask(urls, numberOfThreads, i, outputData));
+            threads.add(thread);
             thread.start();
         }
+
+        // Wait for all threads to finish
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Write output data to Excel
+        writeOutputToExcel(outputData, "output_data.xlsx");
     }
 
     public static List<String> readUrlsFromExcel(String filePath) throws IOException {
@@ -52,7 +75,9 @@ public class Multithread {
                 row.createCell(0).setCellValue(entry.get("url"));
                 row.createCell(1).setCellValue(entry.get("title"));
                 row.createCell(2).setCellValue(entry.get("meta"));
+
             }
+            
 
             try (FileOutputStream fos = new FileOutputStream(filePath)) {
                 workbook.write(fos);
@@ -60,23 +85,24 @@ public class Multithread {
         }
     }
 
-    // Runnable task for each thread
+
     static class BrowserTask implements Runnable {
 
         private List<String> urls;
         private int numberOfThreads;
         private int threadIndex;
+        private List<Map<String, String>> outputData;
 
-        public BrowserTask(List<String> urls, int numberOfThreads, int threadIndex) {
+        public BrowserTask(List<String> urls, int numberOfThreads, int threadIndex, List<Map<String, String>> outputData) {
             this.urls = urls;
             this.numberOfThreads = numberOfThreads;
             this.threadIndex = threadIndex;
+            this.outputData = outputData;
         }
 
         @Override
         public void run() {
             WebDriver driver = new ChromeDriver();
-            List<Map<String, String>> outputData = new ArrayList<>();
             for (int i = threadIndex; i < urls.size(); i += numberOfThreads) {
                 String url = urls.get(i);
                 driver.get(url);
@@ -85,21 +111,18 @@ public class Multithread {
                 try {
                     meta = driver.findElement(By.xpath("//meta[@name='description']")).getAttribute("content");
                 } catch (NoSuchElementException e) {
-
+                    // Handle exception
                 }
                 Map<String, String> data = new HashMap<>();
                 data.put("url", url);
                 data.put("title", title);
                 data.put("meta", meta);
-                outputData.add(data);
+                synchronized (outputData) {
+                    outputData.add(data);
+                }
                 System.out.println("Processed URL " + url + " by " + Thread.currentThread().getName());
             }
             driver.quit();
-            try {
-                writeOutputToExcel(outputData, "output_data_" + threadIndex + ".xlsx");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
